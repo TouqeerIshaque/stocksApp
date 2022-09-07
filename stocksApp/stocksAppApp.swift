0fc8735dbb2397a82ffbbd50c551577d2c623ba2
Muhammad Touqeer Ishaque <touqeerishaque@outlook.com>
2022-09-07 14:18:19 -0700

    Initial Commit

---
+++ stocksApp/stocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  stocksAppApp.swift
+//  stocksApp
+//
+//  Created by Touqeer's Macbook  on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct stocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
